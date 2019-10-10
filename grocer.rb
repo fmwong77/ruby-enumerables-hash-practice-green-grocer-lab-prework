e69be0d7cd27c958df7163033649c949e001d923
Mei Wong <fongmeiw@gmail.com>
2019-10-10 17:36:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,59 @@
 def consolidate_cart(cart)
   # code here
+  groceries = {}
+  
+  cart.each do |grocery|
+    grocery.each do |item, item_hash|
+      groceries[item] ||= item_hash
+      
+      if groceries[item].key?(:count)
+        groceries[item][:count] += 1
+      else
+        groceries[item][:count] = 1
+      end
+    end
+  end
+  groceries
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupon_count = 0
+  pre_coupon = ""
+  coupons.each do |c|
+    if pre_coupon == "" || c[:item] != pre_coupon
+      coupon_count = 0
+    end
+    
+    if cart.key?(c[:item])
+      coupon_count += c[:num]
+      hash = {}
+      hash[:price] = c[:cost] / c[:num]
+      hash[:clearance] = cart[c[:item]][:clearance]
+      hash[:count] = coupon_count
+      cart["#{c[:item]} W/COUPON"] = hash
+
+      if cart[c[:item]][:count] > c[:num]
+        cart[c[:item]][:count] -= c[:num]
+      else
+        cart[c[:item]][:count] = 0
+      end
+      
+    end
+    pre_coupon = c[:item]
+  end
+  cart
 end
 
 def apply_clearance(cart)
   # code here
+  cart.each do |key, value|
+    if cart[key][:clearance]
+    # puts key
+      cart[key][:price] = (cart[key][:price] * 20) / 100
+    end
+  end
+
+  cart
 end
 
 def checkout(cart, coupons)
